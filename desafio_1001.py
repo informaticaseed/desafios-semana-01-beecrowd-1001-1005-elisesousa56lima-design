0fb4df7e83f8e828b49360234c54309583044f27
elisesousa56lima-design <elisesousa56lima@gmail.com>
2026-02-26 09:21:29 -0300

    Update desafio_1001.py

--- desafio_1001.py
+++ desafio_1001.py
@@ -3,11 +3,7 @@ Beecrowd 1001 - Extremamente Básico
 
 Beecrowd 1001 - Extremamente Básico
 """
-# Escreva sua solução abaixoPresentation Error".
-Imprima X conforme exemplo apresentado abaixo.
-não esqueça de imprimir o fim de linha após o resultado, caso contrário,
-você receberá  "Erro de apresentação".
-"""
+
 
 # Link do problema: https://judge.beecrowd.com/pt/problems/view/1001
 
